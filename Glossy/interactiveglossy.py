9ffbc9ce98c94992cce5c0f65a8e9c139f701ece
loud96 <92132196+loud96@users.noreply.github.com>
2021-10-31 02:06:56 -0400

    Add files via upload

---
+++ Glossy/interactiveglossy.py
@@ -0,0 +1,53 @@
+import sys
+from configure import auth_key
+import requests
+import pprint
+from time import sleep
+
+
+path =input("Enter video/audio path: ")
+
+#global constants
+headers = {
+   "authorization": auth_key,
+   "content-type": "application/json"
+}
+transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
+upload_endpoint = 'https://api.assemblyai.com/v2/upload'
+ 
+#Function to pass the file to the upload endpoint
+def read_file(filename):
+   with open(filename, 'rb') as _file:
+       while True:
+           data = _file.read(5242880)
+           if not data:
+               break
+           yield data
+ 
+#upload audio/video file
+upload_response = requests.post(
+   upload_endpoint,
+   headers=headers, data=read_file(""+path+"")
+)
+print('Audio file uploaded')
+ 
+#send the transcription request 
+transcript_request = {'audio_url': upload_response.json()['upload_url'], 
+"iab_categories": True,
+"auto_highlights": True
+}
+transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
+print('Transcription Requested')
+pprint.pprint(transcript_response.json())
+
+#polling
+polling_response = requests.get(transcript_endpoint+"/"+transcript_response.json()['id'], headers=headers)
+filename = transcript_response.json()['id'] + '.txt'
+# if our status isn’t complete or encounters an error, sleep and then poll again
+while polling_response.json()['status'] != 'completed' or ['status'] == 'error':
+   sleep(30)
+   polling_response = requests.get(transcript_endpoint+"/"+transcript_response.json()['id'], headers=headers)
+   print("File is", polling_response.json()['status'])
+with open(filename, 'w') as f:
+   f.write(polling_response.json()['text'])
+print('Transcript saved to', filename)
