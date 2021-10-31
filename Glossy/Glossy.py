40bdf5cb351da553a8247901b40eabe515184bde
loud96 <92132196+loud96@users.noreply.github.com>
2021-10-31 01:57:33 -0400

    Update Glossy.py

--- Glossy/Glossy.py
+++ Glossy/Glossy.py
@@ -4,7 +4,7 @@ import requests
 import pprint
 from time import sleep
  
-# store global constants
+#global constants
 headers = {
    "authorization": auth_key,
    "content-type": "application/json"
@@ -12,7 +12,7 @@ headers = {
 transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
 upload_endpoint = 'https://api.assemblyai.com/v2/upload'
  
-# make a function to pass the mp3 to the upload endpoint
+#Function to pass the mp4 to the upload endpoint
 def read_file(filename):
    with open(filename, 'rb') as _file:
        while True:
@@ -21,22 +21,23 @@ def read_file(filename):
                break
            yield data
  
-# upload our audio file
+#upload audio/video file
 upload_response = requests.post(
    upload_endpoint,
-   headers=headers, data=read_file("/Downloads/math0100.mp4")
+   headers=headers, data=read_file("/Downloads/eng.mp4")
 )
 print('Audio file uploaded')
  
-# send a request to transcribe the audio file
-transcript_request = {'audio_url': upload_response.json()['upload_url'], "iab categories": True}
+#send the transcription request 
+transcript_request = {'audio_url': upload_response.json()['upload_url'], "iab_categories": True}
 transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
 print('Transcription Requested')
 pprint.pprint(transcript_response.json())
-# set up polling
+
+#polling
 polling_response = requests.get(transcript_endpoint+"/"+transcript_response.json()['id'], headers=headers)
 filename = transcript_response.json()['id'] + '.txt'
-# if our status isn’t complete, sleep and then poll again
+# if our status isn’t complete or encounters an error, sleep and then poll again
 while polling_response.json()['status'] != 'completed' or ['status'] == 'error':
    sleep(30)
    polling_response = requests.get(transcript_endpoint+"/"+transcript_response.json()['id'], headers=headers)
